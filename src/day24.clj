de7c5ae3d9419de896297aa1424f00a86010fc79
Espen Amble Kolstad <espen.amble.kolstad@finn.no>
2020-12-24 10:45:47 +0100

    Day24 solution - part-1

---
+++ src/day24.clj
@@ -0,0 +1,35 @@
+(ns day24
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(def input (-> (.getName *ns*) (str ".txt") io/resource slurp))
+
+(defn parse-input [s]
+  (->> s (str/split-lines)
+       (map (fn [s] (map keyword (re-seq #"e|w|ne|nw|se|sw" s))))))
+
+(def dir {:e [-1 1 0] :w [1 -1 0]
+          :ne [0 1 -1] :sw [0 -1 1]
+          :se [-1 0 1] :nw [1 0 -1]})
+
+(defn part-1 [s]
+  (->> (parse-input s)
+       (map (fn [ds] (reduce (fn [p d] (mapv + p (dir d))) [0 0 0] ds)))
+       frequencies
+       (filter (comp odd? val))
+       count))
+
+(comment
+  (part-1 "sesenwnenenewseeswwswswwnenewsewsw\nneeenesenwnwwswnenewnwwsewnenwseswesw\nseswneswswsenwwnwse\nnwnwneseeswswnenewneswwnewseswneseene\nswweswneswnenwsewnwneneseenw\neesenwseswswnenwswnwnwsewwnwsene\nsewnenenenesenwsewnenwwwse\nwenwwweseeeweswwwnwwe\nwsweesenenewnwwnwsenewsenwwsesesenwne\nneeswseenwwswnwswswnw\nnenwswwsewswnenenewsenwsenwnesesenew\nenewnwewneswsewnwswenweswnenwsenwsw\nsweneswneswneneenwnewenewwneswswnese\nswwesenesewenwneswnwwneseswwne\nenesenwswwswneneswsenwnewswseenwsese\nwnwnesenesenenwwnenwsewesewsesesew\nnenewswnwewswnenesenwnesewesw\neneswnwswnwsenenwnwnwwseeswneewsenese\nneswnwewnwnwseenwseesewsenwsweewe\nwseweeenwnesenwwwswnew")
+
+  (part-1 input))
+;495
+
+(defn part-2 [s]
+  (parse-input s))
+
+(comment
+  (part-2 "sesenwnenenewseeswwswswwnenewsewsw\nneeenesenwnwwswnenewnwwsewnenwseswesw\nseswneswswsenwwnwse\nnwnwneseeswswnenewneswwnewseswneseene\nswweswneswnenwsewnwneneseenw\neesenwseswswnenwswnwnwsewwnwsene\nsewnenenenesenwsewnenwwwse\nwenwwweseeeweswwwnwwe\nwsweesenenewnwwnwsenewsenwwsesesenwne\nneeswseenwwswnwswswnw\nnenwswwsewswnenenewsenwsenwnesesenew\nenewnwewneswsewnwswenweswnenwsenwsw\nsweneswneswneneenwnewenewwneswswnese\nswwesenesewenwneswnwwneseswwne\nenesenwswwswneneswsenwnewswseenwsese\nwnwnesenesenenwwnenwsewesewsesesew\nnenewswnwewswnenesenwnesewesw\neneswnwswnwsenenwnwnwwseeswneewsenese\nneswnwewnwnwseenwseesewsenwsweewe\nwseweeenwnesenwwwswnew")
+
+  (part-2 input))
+;
