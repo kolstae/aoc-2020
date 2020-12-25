3e43c0e939bdf5382fadead2c62397fb7b13635c
Espen Amble Kolstad <espen.amble.kolstad@finn.no>
2020-12-25 01:09:06 +0100

    Day24 solution - part-2

--- src/day24.clj
+++ src/day24.clj
@@ -12,12 +12,15 @@
           :ne [0 1 -1] :sw [0 -1 1]
           :se [-1 0 1] :nw [1 0 -1]})
 
-(defn part-1 [s]
+(defn black-tiles [s]
   (->> (parse-input s)
        (map (fn [ds] (reduce (fn [p d] (mapv + p (dir d))) [0 0 0] ds)))
        frequencies
        (filter (comp odd? val))
-       count))
+       keys))
+
+(defn part-1 [s]
+  (count (black-tiles s)))
 
 (comment
   (part-1 "sesenwnenenewseeswwswswwnenewsewsw\nneeenesenwnwwswnenewnwwsewnenwseswesw\nseswneswswsenwwnwse\nnwnwneseeswswnenewneswwnewseswneseene\nswweswneswnenwsewnwneneseenw\neesenwseswswnenwswnwnwsewwnwsene\nsewnenenenesenwsewnenwwwse\nwenwwweseeeweswwwnwwe\nwsweesenenewnwwnwsenewsenwwsesesenwne\nneeswseenwwswnwswswnw\nnenwswwsewswnenenewsenwsenwnesesenew\nenewnwewneswsewnwswenweswnenwsenwsw\nsweneswneswneneenwnewenewwneswswnese\nswwesenesewenwneswnwwneseswwne\nenesenwswwswneneswsenwnewswseenwsese\nwnwnesenesenenwwnenwsewesewsesesew\nnenewswnwewswnenesenwnesewesw\neneswnwswnwsenenwnwnwwseeswneewsenese\nneswnwewnwnwseenwseesewsenwsweewe\nwseweeenwnesenwwwswnew")
@@ -25,11 +28,33 @@
   (part-1 input))
 ;495
 
+(defn surrounding [t]
+  (map #(mapv + t %) (vals dir)))
+
+(defn flip [tiles]
+  (let [t->ns (map (juxt identity surrounding) tiles)
+        ts (->> t->ns
+                (remove (fn [[_ ns]] (let [cnt (count (filter tiles ns))] (or (zero? cnt) (< 2 cnt)))))
+                (map first)
+                set)
+        nfs (reduce (fn [fs [_ ns]]
+                      (reduce #(update %1 %2 (fnil inc 0)) fs (remove tiles ns)))
+                    {} t->ns)]
+    (into ts
+          (comp (filter (comp #(= 2 %) val))
+                (map key))
+          nfs)))
+
 (defn part-2 [s]
-  (parse-input s))
+  (->> (black-tiles s)
+       set
+       (iterate flip)
+       (drop 100)
+       (map count)
+       first))
 
 (comment
   (part-2 "sesenwnenenewseeswwswswwnenewsewsw\nneeenesenwnwwswnenewnwwsewnenwseswesw\nseswneswswsenwwnwse\nnwnwneseeswswnenewneswwnewseswneseene\nswweswneswnenwsewnwneneseenw\neesenwseswswnenwswnwnwsewwnwsene\nsewnenenenesenwsewnenwwwse\nwenwwweseeeweswwwnwwe\nwsweesenenewnwwnwsenewsenwwsesesenwne\nneeswseenwwswnwswswnw\nnenwswwsewswnenenewsenwsenwnesesenew\nenewnwewneswsewnwswenweswnenwsenwsw\nsweneswneswneneenwnewenewwneswswnese\nswwesenesewenwneswnwwneseswwne\nenesenwswwswneneswsenwnewswseenwsese\nwnwnesenesenenwwnenwsewesewsesesew\nnenewswnwewswnenesenwnesewesw\neneswnwswnwsenenwnwnwwseeswneewsenese\nneswnwewnwnwseenwseesewsenwsweewe\nwseweeenwnesenwwwswnew")
 
   (part-2 input))
-;
+;4012
