8a441f790fdd7b2262f84ee1c84de8ab01a4d9bc
Espen Amble Kolstad <espen.amble.kolstad@finn.no>
2020-12-27 11:53:05 +0100

    Day23 solution - real functional

--- src/day23.clj
+++ src/day23.clj
@@ -6,27 +6,41 @@
 (defn parse-input [s]
   (map #(- (int %) (int \0)) s))
 
-(defn take-until [p s] (transduce (halt-when p (fn [r h] (conj r h))) conj [] s))
+(defn ptr->seq [c ptr] (take (count ptr) (iterate #(get ptr (dec %)) c)))
 
-(defn do-turn [m]
-  (fn [[c & ns]]
-   (let [[ps ns] (split-at 3 ns)
-         dest (first (remove (into #{0} ps) (iterate #(mod (dec %) m) (dec c))))
-         head (take-until #{dest} ns)
-         tail (drop (count head) ns)]
-     ;(prn c ps :dest dest head tail)
-     (concat head ps tail [c]))))
+(defn do-turn [n]
+  (fn [[c ptr]]
+    (let [f-p (get ptr (dec c))
+          m-p (get ptr (dec f-p))
+          l-p (get ptr (dec m-p))
+          dest (inc (first (remove (comp #{f-p m-p l-p} inc) (next (iterate #(mod (dec %) n) (dec c))))))
+          n-ptr (-> ptr
+                    (assoc (dec c) (get ptr (dec l-p)))
+                    (assoc (dec dest) f-p)
+                    (assoc (dec l-p) (get ptr (dec dest))))]
+      [(get n-ptr (dec c)) n-ptr])))
+
+(defn do-turns [start ptr n]
+  (->> [start ptr]
+       (iterate (do-turn (count ptr)))
+       (drop n)
+       first
+       second))
+
+(defn ns->next-ptr [is]
+  (->> is
+       cycle
+       (partition 2 1)
+       (take (count is))
+       (sort-by first)
+       (mapv second)))
 
 (defn part-1 [s]
-  (->> (parse-input s)
-       (iterate (do-turn 10))
-       (drop 100)
-       first
-       cycle
-       (drop-while (complement #{1}))
-       next
-       (take 8)
-       str/join))
+  (let [is (parse-input s)]
+    (->> (do-turns (first is) (ns->next-ptr is) 100)
+         (ptr->seq 1)
+         next
+         str/join)))
 
 (comment
   (part-1 "389125467")
@@ -35,18 +49,15 @@
 ;"54327968"
 
 (defn part-2 [s n]
-  (->> (concat (parse-input s) (range 10 1000001))
-       (iterate (do-turn 1000001))
-       (drop n)
-       first
-       cycle
-       (drop-while (complement #{1}))
-       next
-       (take 2)
-       (apply *)))
+  (let [is (concat (parse-input s) (range 10 1000001))]
+    (->> (do-turns (first is) (ns->next-ptr is) n)
+         (ptr->seq 1)
+         next
+         (take 2)
+         (apply *))))
 
 (comment
-  (time (part-2 "389125467" 1000))
+  (time (part-2 "389125467" 10000000))
 
   (part-2 input 10000000))
-;
+;157410423276
